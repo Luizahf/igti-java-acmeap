1e0be0246980a7926e3e0a2871b5d4c8de1eb648
Luizahf <luizahfreire@hotmail.com>
2020-08-04 20:53:23 -0300

    Project implementation

---
+++ demo-acme-ap/src/main/java/com/luizafreire/demoacmeap/com.luizafreire.demoacmeap.exception/RecursoNotFoundException.java
@@ -0,0 +1,9 @@
+package com.luizafreire.demoacmeap.exception;
+
+public class RecursoNotFoundException extends RuntimeException {
+
+	public RecursoNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
